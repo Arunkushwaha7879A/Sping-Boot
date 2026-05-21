37738b9003285614cd438d77ca669343a64a458d
Arun Kushwaha <arun787947@gmail.com>
2026-05-21 23:42:49 +0530

    27commit

--- substring-foodie/src/main/java/com/substring/foodie/substring_foodie/Example/AuthController.java
+++ substring-foodie/src/main/java/com/substring/foodie/substring_foodie/Example/AuthController.java
@@ -1,13 +1,13 @@
 package com.substring.foodie.substring_foodie.Example;
 
 import com.substring.foodie.substring_foodie.payload.UserData;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,20 +15,24 @@ public class AuthController {
 
     private Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    public void login(){
+//    @RequestMapping(value = "/login" , method = RequestMethod.POST  )
+    @PostMapping("/login")
+    public Map<String , Object>login(@RequestBody Map<String,Object>data , @RequestHeader("User-Agent") String agent){
 
+        logger.info("login request : {}" , data);
+        logger.info(" agent : {}" , agent);
+        return data;
     }
 
 
     @RequestMapping("/signup")
-    public String signup(@RequestBody List<String> Data){
-//        logger.info("user name:{}" , userData.getName());
-//        logger.info("age :{}" , userData.getAge());
-//        logger.info("password :{}" , userData.getPassword());
-//        logger.info("password :{}" , userData.getEmail());
-        logger.info("name:{}" , Data.get(0));
+    public String signup(@Valid @RequestBody  UserData userData){
+        logger.info("user name:{}" , userData.getName());
+        logger.info("age :{}" , userData.getAge());
+        logger.info("password :{}" , userData.getPassword());
+        logger.info("password :{}" , userData.getEmail());
+//        logger.info("name:{}" , Data.get(0));
         return "we got data";
-
     }
 
 }
--- substring-foodie/src/main/java/com/substring/foodie/substring_foodie/payload/UserData.java
+++ substring-foodie/src/main/java/com/substring/foodie/substring_foodie/payload/UserData.java
@@ -1,9 +1,17 @@
 package com.substring.foodie.substring_foodie.payload;
 
+import jakarta.validation.constraints.*;
+
 public class UserData {
+    @NotEmpty(message="name is required!!")
+    @Size(min=3 ,max=20 , message="Name must be between 2 and 20 chararcters")
     private String name;
+    @Min(value = 18 , message = "minimum value required id 18")
+    @Max(value = 99 , message = "maximum value required id 18")
     private int age;
+    @Email(message = "invalid email !!")
     private String email;
+    @NotEmpty(message = "password is required")
     private String password;
 
     public String getName() {
