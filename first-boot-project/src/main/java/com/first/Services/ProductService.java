40894d3212fe20f7e96840138a57e8bc3878cc49
Arun Kushwaha <arun787947@gmail.com>
2026-06-03 22:04:52 +0530

    updated

--- first-boot-project/src/main/java/com/first/Services/ProductService.java
+++ first-boot-project/src/main/java/com/first/Services/ProductService.java
@@ -3,41 +3,42 @@ package com.first.Services;
 import org.springframework.stereotype.Service;
 
 @Service
-//public class ProductService {
-//
-//    public void createProduct(){
-//        System.out.println("creating product");
-//        System.out.println("product created");
-//    }
-//
-//    //craete
-////product ko app kcreate kar rhe ho - core logic
-//
-//
-////logging
-//
-//
-////security
-//
-////transaction management
-//
-//
-//    //get
-//
-//    public  void gettingProduct(){
-//        System.out.println("getting product");
-//        System.out.println("Got");
-//    }
-//
-//    //search
-//
-//
-//    public void searchProduct(){
-//        System.out.println("searching product");
-//        System.out.println("got");
-//    }
+public class ProductService {
 
-//qsbjbjhjhsqxbsxbjkbsx
+    public void createProduct(){
+        System.out.println("creating product");
+        System.out.println("product created");
+    }
+
+    //craete
+//product ko app kcreate kar rhe ho - core logic
+
+
+//logging
+
+
+//security
+
+//transaction management
+
+
+    //get
+
+    public  void gettingProduct(){
+        System.out.println("getting product");
+        System.out.println("Got");
+    }
+
+    //search
+
+
+    public void searchProduct(){
+        System.out.println("searching product");
+        System.out.println("got");
+    }
+
+//qsbjbjhjhsqxbsxbjkbsxsdflmvvds
+    //sdvjxkcvsjljvlvjevljvf
 
 
 
