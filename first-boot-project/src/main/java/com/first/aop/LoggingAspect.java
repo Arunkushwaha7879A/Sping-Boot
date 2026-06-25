ac5ac269dea0a7a3738ba712392f3bf50f2924df
Arun Kushwaha <arun787947@gmail.com>
2026-06-25 22:07:24 +0530

    26june

--- first-boot-project/src/main/java/com/first/aop/LoggingAspect.java
+++ first-boot-project/src/main/java/com/first/aop/LoggingAspect.java
@@ -1,24 +1,27 @@
-//package com.first.aop;
-//
-//import org.aspectj.lang.annotation.After;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.springframework.stereotype.Component;
-//
-//@Aspect
-//@Component
-//public class LoggingAspect {
-//
-//    //cross cutting concern
-//    @Before("execution(* com.first.Services.ProductService.*(..))")
-//    public void greeting(){
-//        System.out.println("hii method starting.....");
-//    }
-//
-//    @After("execution(* com.first.Services.ProductService.createProduct(..))")
-//    public void byebye(){
-//        System.out.println("bye bye terminating method");
-//    }
+package com.first.aop;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    //cross cutting concern
+    @Before("execution(* com.first.Services.ProductService.*(..))")
+    public void greeting(){
+        System.out.println("hii method starting.....");
+    }
+
+    @After("execution(* com.first.Services.ProductService.createProduct(..))")
+    public void byebye(){
+        System.out.println("bye bye terminating method");
+    }
 
 ggggggvtvgr
+        abjshjbabjnsuansas
+        shuhusbhjsbjhsbhbhjbhjbhjugyfscvtyvsty
+                sghvbhjs
 gvrfvfr
