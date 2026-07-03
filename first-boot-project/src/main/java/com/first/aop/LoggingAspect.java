cb8f75d80401b94e9a6543c58a99eef371430283
Arun Kushwaha <arun787947@gmail.com>
2026-07-03 23:01:42 +0530

    3july

--- first-boot-project/src/main/java/com/first/aop/LoggingAspect.java
+++ first-boot-project/src/main/java/com/first/aop/LoggingAspect.java
@@ -1,36 +1,44 @@
-//package com.first.aop;
-////
-////import org.aspectj.lang.annotation.After;
-////import org.aspectj.lang.annotation.Aspect;
-////import org.aspectj.lang.annotation.Before;
-////import org.springframework.stereotype.Component;
-////
-////@Aspect
-////@Component
-////public class LoggingAspect {
-////
-////    //cross cutting concern
-////    @Before("execution(* com.first.Services.ProductService.*(..))")
-////    public void greeting(){
-////        System.out.println("hii method starting.....");
+////package com.first.aop;
+//////
+//////import org.aspectj.lang.annotation.After;
+//////import org.aspectj.lang.annotation.Aspect;
+//////import org.aspectj.lang.annotation.Before;
+//////import org.springframework.stereotype.Component;
+//////
+//////@Aspect
+//////@Component
+//////public class LoggingAspect {
+//////
+//////    //cross cutting concern
+//////    @Before("execution(* com.first.Services.ProductService.*(..))")
+//////    public void greeting(){
+//////        System.out.println("hii method starting.....");
+//////    }
+//////
+//////    @After("execution(* com.first.Services.ProductService.createProduct(..))")
+////    public void byebye(){
+////        System.out.println("bye bye terminating method");
 ////    }
-////
-////    @After("execution(* com.first.Services.ProductService.createProduct(..))")
-//    public void byebye(){
-//        System.out.println("bye bye terminating method");
-//    }
+//
+//dijewjiwecdwcdvscdsdwfdvdsvvddwcwdcw
+//////
+////////dfvjnvdsjndvsnjgbbhtbth
+//////tggbtbgtbgfd
+//////frfvrtg
+//////        fefrvfdf
+//////erfefrve
+//////efrefdfvddd
+//////        dsdcsvdsvds
+//////fewsdbhbhs
+////cwbjcdjwjdcnjnjkcdk
+////edbjhdhbjcbcdwbnjnkcdv
+////        ewdhvcbdhjwcdwcnjd
+////ewbh
 
-dijewjiwecdwcdvscdsdwfdvdsvvddwcwdcw
-////
-//////dfvjnvdsjndvsnjgbbhtbth
-////tggbtbgtbgfd
-////frfvrtg
-////        fefrvfdf
-////erfefrve
-////efrefdfvddd
-////        dsdcsvdsvds
-////fewsdbhbhs
-//cwbjcdjwjdcnjnjkcdk
-//edbjhdhbjcbcdwbnjnkcdv
-//        ewdhvcbdhjwcdwcnjd
-//ewbhjcdej
+bhhhjnjcdejhbbhbh
+jbnnjnbhjbhb
+        jhnjjb
+hbbnjnjnjk
+                jnnjnjnjnj
+hjjnjn
+                   i     jnjn
